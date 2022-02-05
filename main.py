a82cd4fbb74f12c9c2edce5ace9ba6df3266544a
SamuelSilveira08 <samuel.santossilveira08@gmail.com>
2022-02-05 19:03:54 -0300

    Adding feature that detects if attack is available, with pytesseract, and some other changes to avoid bugs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pyautogui
 import time
+import pytesseract
+
 from itertools import repeat
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
 def click_btn(img, timeout=6):
@@ -11,13 +15,12 @@ def click_btn(img, timeout=6):
             pyautogui.moveTo(pos, duration=0.6)
             pyautogui.click()
             time.sleep(2)
-            break
+            return pos
         else:
             if time.time() - start < timeout:
                 continue
             else:
-                x = -1
-                y = -1
+                pos = pyautogui.Point(-1, -1)
                 return pos
 
 
@@ -27,26 +30,38 @@ def attack(players):
     for i in players:
         aux.extend(repeat(i, 3))
     for player in aux:
-        print('running {0}'.format(player))
-        click_btn('img/ranking.png')
-        click_btn('img/search-bar.png')
-        pyautogui.moveRel(50, 0)
-        pyautogui.keyDown('backspace')
-        time.sleep(2)
-        pyautogui.keyUp('backspace')
-        pyautogui.write(player)
-        pyautogui.moveRel(0, 50)
-        click_btn('img/search-btn.png')
-        click_btn('img/show-hero-btn.png')
-        click_btn('img/attack-btn.png')
-        click_btn('img/ok-btn.png')
-        click_btn('img/discount.png')
-        click_btn('img/catch-btn.png')
-        click_btn('img/close-btn.png')
-        count += 1
-        if count == 3:
-            count = 0
-        time.sleep(300)
+        while True:
+            pos = click_btn('img/courage-bar.png')
+            x = pos[0]
+            y = pos[1]
+            time.sleep(2)
+            image = pyautogui.screenshot(region=(x - 40, y - 10, 130, 25))
+            courage = pytesseract.image_to_string(image)
+            if int(courage) >= 16:
+                print('running {0}'.format(player))
+                click_btn('img/ranking.png')
+                click_btn('img/hero-rank-btn.png')
+                click_btn('img/search-bar.png')
+                pyautogui.moveRel(50, 0)
+                pyautogui.keyDown('backspace')
+                time.sleep(2)
+                pyautogui.keyUp('backspace')
+                pyautogui.write(player)
+                pyautogui.moveRel(0, 50)
+                click_btn('img/search-btn.png')
+                click_btn('img/show-hero-btn.png')
+                click_btn('img/attack-btn.png')
+                click_btn('img/ok-btn.png')
+                click_btn('img/discount.png')
+                click_btn('img/catch-btn.png')
+                click_btn('img/close-btn.png')
+                count += 1
+                if count == 3:
+                    count = 0
+                time.sleep(480)
+                break
+            else:
+                time.sleep(10)
 
 
 def load_players(file):
@@ -58,4 +73,3 @@ def load_players(file):
 players = load_players('players.txt')
 players = map(lambda player: player.rstrip('\n'), players)
 attack(players)
-
